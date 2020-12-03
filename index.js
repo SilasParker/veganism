b5ac8c7b35abf8a97d66e9aa691fcf91e6436c6d
SilasParker <silas.parker@gmail.com>
2020-12-03 14:44:01 +0000

    Styling and minor functionality addition
    CSS: Styled buttons, changed font, adapted some font sizes, re-organised the document and styled the search bar
    HTML: Added new Google Font, gave a couple elements class names
    Index.js: added ability to press enter instead of clicking search

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ function initMap() {
         center: new google.maps.LatLng(50.835160, -0.137110), zoom: 15,
     };
     map = new google.maps.Map(document.getElementById("map"), mapProp);
+    document.getElementById("search-bar").addEventListener("keyup",function(event) {
+        if(event.key === "Enter") {
+            event.preventDefault();
+            document.getElementById("search-submit").click();
+        }
+    });
 
 
 }
